c39029dde427c3b2d31cf9f60e3a880d4fbc8e37
ttimbers <tiffany.timbers@gmail.com>
2015-02-22 16:38:47 -0800

    Fixed some bugs in generation of toy data, and made a second script called toydata-generation.R that doesn't include code for clustering

--- toydata-generation.R
+++ toydata-generation.R
@@ -26,7 +26,7 @@ make.toy.data <- function(means, stdevs, N) {
     dim1[(previous.N+1):((previous.N)+N)]  <- temp
     previous.N  <- N + previous.N
   }
- 
+  
   ##for each cluster, generate toy data and save them in dim2
   previous.N  <- 0
   for (i in 1:cluster.n){
@@ -76,70 +76,9 @@ my.toy.df.matrix <- as.matrix(my.toy.df)
 
 ##plot unclustered data
 library(scatterplot3d)
-par(mfrow=c(1,1))
 scatterplot3d(my.toy.df.matrix)
 
 par(mfrow=c(1,3))
 plot(my.toy.df.matrix[,1], my.toy.df.matrix[,2])
 plot(my.toy.df.matrix[,2], my.toy.df.matrix[,3])
 plot(my.toy.df.matrix[,3], my.toy.df.matrix[,1])
-
-##do K-means clustering on toy dataset
-km.out = kmeans(my.toy.df.matrix, 3, nstart = 100)
-km.out
-
-##replot and colour code clusters
-scatterplot3d(my.toy.df.matrix, color = km.out$cluster, pch = 19)
-
-
-##SOM on same data 
-library(kohonen)
-
-# Also center and scale all variables
-my.toy.df.matrix <- as.matrix(scale(my.toy.df))
-
-##Create the SOM Grid - you generally have to specify the size of the 
-##training grid prior to training the SOM. Hexagonal and Circular 
-##topologies are possible
-som_grid <- somgrid(xdim = 20, ydim=20, topo="hexagonal")
-
-##Train the SOM, options for the number of iterations,
-##the learning rates, and the neighbourhood are available
-som_model <- som(my.toy.df.matrix, grid=som_grid, rlen=100, alpha=c(0.05,0.01), keep.data = TRUE, n.hood='circular')
-
-##visualise the quality of your generated SOM
-plot(som_model, type="changes")
-
-##visualise the count of how many samples are mapped to each node on the map
-plot(som_model, type="count")
-
-##“U-Matrix”, this visualisation is of the distance between each node and its neighbours
-plot(som_model, type="dist.neighbours")
-
-##visualise the node weight vectors, or “codes”, which are made up of normalised  
-##values of the original variables used to generate the SOM
-plot(som_model, type="codes")
-
-##visualize heatmap (note-these are scaled and centred)
-coolBlueHotRed <- function(n, alpha = 1) { rainbow(n, end=4/6, alpha=alpha)[n:1] }
-par(mfrow=c(1,3))
-plot(som_model, type = "property", property = som_model$codes[,1], main=names(som_model$data)[4], palette.name=coolBlueHotRed)
-plot(som_model, type = "property", property = som_model$codes[,2], main=names(som_model$data)[4], palette.name=coolBlueHotRed)
-plot(som_model, type = "property", property = som_model$codes[,3], main=names(som_model$data)[4], palette.name=coolBlueHotRed)
-
-##Cluster SOM data
-mydata <- som_model$codes 
-wss <- (nrow(mydata)-1)*sum(apply(mydata,2,var)) 
-for (i in 2:15) {
-  wss[i] <- sum(kmeans(mydata, centers=i)$withinss)
-}
-par(mfrow=c(1,1))
-plot(wss)
-## use hierarchical clustering to cluster the codebook vectors
-som_cluster <- cutree(hclust(dist(som_model$codes)), 3)
-##make a pretty colour pallete
-pretty.colours  <- c("thistle1", "tomato1", "turquoise", "slategray1", "sienna1", "violet", "skyblue")
-##plot these results:
-plot(som_model, type="mapping", bgcol = pretty.colours[som_cluster], main = "Clusters") 
-add.cluster.boundaries(som_model, som_cluster)
-
